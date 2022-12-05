5628fca8c59b24e700df4d5cb60e1be7226e32ea
Reizo.Hosoda <61483910+reizo0901@users.noreply.github.com>
2022-12-05 22:04:54 +0900

    Add files via upload

---
+++ mksisetu.bat
@@ -0,0 +1,22 @@
+mkdir c:\sharedfiles\
+mkdir c:\sharedfiles\施設部_00_総務担当管理用
+mkdir c:\sharedfiles\施設部_01_統括グループ
+mkdir c:\sharedfiles\施設部_02_不動産サービス事業グループ
+mkdir c:\sharedfiles\施設部_03_プロジェクト管理グループ
+mkdir c:\sharedfiles\施設部_04_品質管理グループ
+mkdir c:\sharedfiles\施設部_05_開発設計グループ
+mkdir c:\sharedfiles\施設部_06_建築計画グループ
+mkdir c:\sharedfiles\施設部_07_施設保全グループ
+mkdir c:\sharedfiles\施設部_08_その他のＦＳＳ
+mkdir c:\sharedfiles\施設部_09_ＤＸ推進グループ
+mkdir c:\sharedfiles\施設部_10_北海道施設センター
+mkdir c:\sharedfiles\施設部_20_東北施設センター
+mkdir c:\sharedfiles\施設部_30_首都圏施設センター
+mkdir c:\sharedfiles\施設部_40_中部施設センター
+mkdir c:\sharedfiles\施設部_50_近畿施設センター
+mkdir c:\sharedfiles\施設部_60_中四国施設センター
+mkdir c:\sharedfiles\施設部_70_九州施設センター
+mkdir c:\sharedfiles\施設部_99_施設部・施設センター共用
+mkdir c:\sharedfiles\施設部_DX推進グループ
+pause
+exit
